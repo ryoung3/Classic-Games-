a6c2d057e389c577b529c01029b21fd7de43c0ae
Ronique Young <youngronique09@gmail.com>
2019-01-05 03:24:33 -0500

    Created the Player class for Tic-Tac-Toe

---
+++ Player.java
@@ -0,0 +1,18 @@
+public class Player{
+
+    public String firstName;
+    private char letter;
+
+    public Player(String firstName, char letter){
+        this.firstName = firstName;
+        this.letter = letter;
+    }
+
+    public String getName(){
+        return this.firstName;
+    }
+
+    public char getLetter(){
+        return this.letter;
+    }
+}
