e1de0587ac4aa5f102cbd7ba476178fdd9112725
Ronique Young <youngronique09@gmail.com>
2019-01-06 22:10:50 -0500

    created board for tic tac toe

--- classicgames-tictactoe/src/main/java/com/ryoung3/classicgames/tictactoe/TicTacToeBoard.java
+++ classicgames-tictactoe/src/main/java/com/ryoung3/classicgames/tictactoe/TicTacToeBoard.java
@@ -2,5 +2,22 @@ package com.ryoung3.classicgames.tictactoe;
 
 public class TicTacToeBoard{
 
+    private char[][] charBoard = new char[3][3];
+    
+    public void drawBoard(){
+        /*
+             
+                 |     |     
+             - - - - - - - -
+                 |     |     
+             - - - - - - - -
+                 |     |
+             
+        */
+        for(int row = 0; row < 3; row++){
+            for
+        }
+    
+    }
     
 }
