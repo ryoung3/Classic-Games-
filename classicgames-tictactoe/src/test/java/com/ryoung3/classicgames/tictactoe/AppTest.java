67c564be1c715ee90263f6db09bae48f924cc83f
Ronique Young <youngronique09@gmail.com>
2019-01-27 03:42:13 -0500

    Began adding unit tests for tic-tac-toe game

--- classicgames-tictactoe/src/test/java/com/ryoung3/classicgames/tictactoe/AppTest.java
+++ classicgames-tictactoe/src/test/java/com/ryoung3/classicgames/tictactoe/AppTest.java
@@ -2,6 +2,7 @@ package com.ryoung3.classicgames.tictactoe;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -12,7 +13,21 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+    @Before
+static void setup() {
+    TicTacToe TTTgame = new TicTacToe();
+    TicTacToeBoard gameBoard = new TicTacToeBoard();
+    Player player1 = new Player("Bobby", 'X');
+    Player player2 = new Player("Blair", 'O');
+}
+
     @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+     @Test
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
